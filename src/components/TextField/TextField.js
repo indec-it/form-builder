a13ef97a1732e0846a6b508a7d18e2e9d7622c0a
Maximiliano forlenza <forlenzamaximiliano@gmail.com>
2022-11-15 17:32:51 -0300

    fix(formBuilder): fix section

--- src/components/TextField/TextField.js
+++ src/components/TextField/TextField.js
@@ -1,20 +1,23 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import PropTypes from 'prop-types';
 import Box from '@mui/material/Box';
+import InputAdornment from '@mui/material/InputAdornment';
 import MuiTextField from '@mui/material/TextField';
+import Tooltip from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
+import HelpIcon from '@mui/icons-material/Help';
 import {getIn} from 'formik';
 
 import defaultMessages from '@constants/defaultMessages';
 import {formikField, formikForm} from '@utils/propTypes';
 
 function TextField({
-  form, field, placeholder, label, readOnlyMode, ...props
+  form, field, placeholder, label, readOnlyMode, tooltip, ...props
 }) {
   const error = getIn(form.errors, field.name);
   const touched = getIn(form.touched, field.name);
   return (
-    <>
+    <Box sx={{width: '100%'}}>
       <Box mb={0.5}>
         <Typography>{label}</Typography>
       </Box>
@@ -29,9 +32,29 @@ function TextField({
           {...props}
           id={`field-${field.name}`}
           placeholder={placeholder}
+          InputProps={{
+            endAdornment: tooltip && (
+              <InputAdornment position="end">
+                <Tooltip
+                  arrow
+                  placement="top"
+                  title={tooltip}
+                >
+                  <Box
+                    display="flex"
+                    alignItems="center"
+                    flexWrap="wrap"
+                    px={2}
+                  >
+                    <HelpIcon fontSize="small" />
+                  </Box>
+                </Tooltip>
+              </InputAdornment>
+            )
+          }}
         />
       )}
-    </>
+    </Box>
   );
 }
 
@@ -40,11 +63,13 @@ TextField.propTypes = {
   form: formikForm.isRequired,
   readOnlyMode: PropTypes.bool.isRequired,
   label: PropTypes.string.isRequired,
-  placeholder: PropTypes.string
+  placeholder: PropTypes.string,
+  tooltip: PropTypes.string
 };
 
 TextField.defaultProps = {
-  placeholder: '[Ingrese texto]'
+  placeholder: '[Ingrese texto]',
+  tooltip: undefined
 };
 
 export default TextField;
