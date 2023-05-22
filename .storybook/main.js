876d97cd32e5bfc3c11b1fc9859ed3fd57e199e1
Maximiliano forlenza <forlenzamaximiliano@gmail.com>
2023-05-22 09:45:28 -0300

    chore(package): upgrade storybook version

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,25 +1,21 @@
 const path = require('path');
-
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
+  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+  "framework": {
+    name: "@storybook/react-webpack5",
+    options: {}
   },
-  webpackFinal: async (config, { configType }) => {
+  webpackFinal: async (config, {
+    configType
+  }) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': path.resolve(__dirname, "../src"),
+      '@': path.resolve(__dirname, "../src")
     };
-
     return config;
+  },
+  docs: {
+    autodocs: true
   }
-}
+};
